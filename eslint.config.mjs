f097b5165ed29525ff24c6e2b2e2e3490675780e
Ciaran Green <ciarantgreen@gmail.com>
2024-06-28 18:41:05 +0100

    Updated ESLint config with StylisticJs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,17 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
+import stylisticJs from '@stylistic/eslint-plugin-js'
 
 export default withNuxt(
-  // Your custom config here
+  {
+    plugins: {
+      '@stylistic/js': stylisticJs
+    },
+    rules: {
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': ['error', 'never'],
+      '@stylistic/js/comma-dangle': ['error', 'only-multiline'],
+    }
+  },
 )
